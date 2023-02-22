93fe07dca4ba5a6a0a5eab60c4b66339d9389964
Lucas Nunes <lukasnunesj@gmail.com>
2023-02-22 12:02:27 -0300

    Adjusting routes and adding messages in pt-br

--- app/controllers/get_message_controller.rb
+++ app/controllers/get_message_controller.rb
@@ -1,13 +1,36 @@
 class GetMessageController < ApplicationController
   def index
     messages = [
-      'If at first you don\'t succeed, well, that\'s about normal.',
-      'The best things in life are actually really expensive.',
-      'The only difference between try and triumph is a little umph… and failure.',
-      'If you can\'t convince them, confuse them.',
-      'Opportunity may knock, but it sure likes to play hide and seek.',
-      'There\'s no \'I\' in \'team\', but there\'s a \'u\' in \'failure\'.',
-      'Success is just a series of failures, punctuated by brief moments of victory.'
+      'Quando tudo estiver dando errado, se acostume!',
+      'Você não pode mudar o seu passado, mas irá estragar seu futuro.',
+      'É preciso fechar ciclos para fracassar em algo novo.',
+      'Quanto maior o seu esforço, maior o seu fracasso',
+      'Dias ruins se vão para que piores possam vir.',
+      'Não pare até se humilhar!',
+      'Se ainda não deu merda, é porque ainda não acabou!',
+      'Foi difícil chegar até aqui, mesmo não chegando em lugar nenhum.',
+      'Jamais deixe de desistir!',
+      'Nunca é tarde para parar de acreditar.',
+      'Trabalhe enquanto eles herdam!',
+      'A vida é feita de obstáculos, e você tropeça em todos!',
+      'Quem acredita sempre cansa',
+      'Quando te falarem que não vai dar certo, acredite.',
+      'Todo mundo é imcompetente, inclusive você!',
+      'Acorde cedo pra atrasar com calma!',
+      'A maior motivação é o desespero.',
+      'Não desista amanha se você pode desistir hoje!',
+      'Você ainda não chegou lá, mas olhe o quanto você já se fodeu!',
+      'A derrota você já tem, busque a humilhação.',
+      'Não desanime com a derrota de hoje, amanhã tem mais!',
+      'Lute como nunca! Perca como sempre!',
+      'A derrota vem pra quem não desiste!',
+      'O caminho é longo, mas a derrota é certa.',
+      'A vida é sua, estrague-a como quiser.',
+      'Trabalhe com o que você ama, e nunca mais vai amá-lo!',
+      'O trabalho danifica o homem.',
+      'Pare de achar que vai dar tudo errado, tenha certeza!',
+      'Para ficar ruim, tem que melhorar muito!',
+      'Nenhum obstáculo é grande demais para quem desiste.'
     ]
 
     random_message = messages.sample
